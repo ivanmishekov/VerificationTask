c258f27c0154a34a4d9e5ca5feef614ecd0d56c5
Ivan <mishekov6868@gmail.com>
2023-02-28 23:44:49 +0300

    Написал программу, решающую задачу!

--- Task.cs
+++ Task.cs
@@ -1,1 +1,28 @@
-﻿
+﻿string[] firstArray = new string[10] { "One", "Two", "Three", "Four", "Five", "1", "22", "333", "4444", "55555" };
+string[] secondArray = new string[10];
+Solution(firstArray, secondArray);
+PrintArray(secondArray);
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "    ");
+    }
+    Console.WriteLine();
+}
+
+
+void Solution(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
